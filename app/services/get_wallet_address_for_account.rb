654bbeed62fb326936444d8e4ccefb9348b27497
Jonathan Rose <jonr87ee@gmail.com>
2016-01-23 14:23:36 -0500

    FIX wallet address service

--- app/services/get_wallet_address_for_account.rb
+++ app/services/get_wallet_address_for_account.rb
@@ -1,7 +1,10 @@
 class GetWalletAddressForAccount
   def self.call(account_id)
-    # Returns wallet address
     @account = Account.find(account_id)
-    @address = @account ? @account.bitcoin_address : nil
+    if @account
+      @account.bitcoin_address
+    else
+      nil
+    end
   end
 end
