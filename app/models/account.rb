407cfc5be1aa77b579d65563db5a49e9dbeb6407
Sean Sellek <me@seansellek.com>
2016-01-23 12:14:01 -0500

    Added session, account, transaction, user models

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,9 @@
 class Account < ActiveRecord::Base
+  before_create :generate_authentication_token!
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Digest::SHA1.hexdigest Time.now.to_s
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
