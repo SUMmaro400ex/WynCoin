d7aee72548e059e40351a3d56cc127db43b854e8
Sean Sellek <me@seansellek.com>
2016-01-23 12:54:56 -0500

    Facebook id to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
+  protected
+
+  def authenticate
+    if current_user.nil?
+      respond_to do |format|
+        format.html { flash[:error] = 'Not authorized.'; redirect_to '/login' }
+        format.json { render json: { errors: "Not authenticated" }, status: :unauthorized }
+      end
+    end 
+  end
+
+  def current_user
+    if session[:user_id] && session[:user_type] == "user"
+      @current_user = User.find_by(session[:user_id])
+    elsif session[:user_id] && session[:user_type] == "account"
+      @current_user = Account.find_by(session[:user_id])
+    else
+      @current_user = authenticate_with_http_basic { |u, p| Account.find_by(api_key: u) }
+    end
+  end
+
 end
