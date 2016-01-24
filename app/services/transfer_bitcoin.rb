1236ef5cd9c057cf1713677925ac6c480ac1685a
Jonathan Rose <jonr87ee@gmail.com>
2016-01-24 02:06:23 -0500

    FIX big num issue

--- app/services/transfer_bitcoin.rb
+++ app/services/transfer_bitcoin.rb
@@ -5,7 +5,7 @@ class TransferBitcoin
     client = Client.get
     account = client.primary_account
     address = GetWalletAddressForAccount.call(account)
-    btc = CurrencyConverter.call(amount, :satoshi)
+    btc = CurrencyConverter.call(amount.to_digits, :satoshi)
     if address && amount
       account.send(to: address, amount: btc.to_s , currency: "BTC", description: "Payment")
       true
